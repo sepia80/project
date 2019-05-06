529dba91c16f1baad5dc713c5bc8cbb1ba2c7fb0
sepia80 <sepia80@naver.com>
2019-05-06 18:13:15 +0900

    알아서 하세요

--- WhatsUpJava/src/org/sepia/ch01/Out.java
+++ WhatsUpJava/src/org/sepia/ch01/Out.java
@@ -4,6 +4,6 @@ public class Out {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("2019-05-06 18:05");
-		System.out.println("2019-05-06 18:11 gitì—ì„œ ì¶”ê°€");
+		System.out.println("2019-05-06 18:11 git Ãæµ¹½Ã¿¡´Â ¾î¶»°Ô Ç¥½Ã¸¦ ÇØ¾ß ÇÏÁÒ?");
 	}
 }
