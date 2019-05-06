a52152297149167a8d3a349a1ff53ed9278edb8a
sepia80 <sepia80@naver.com>
2019-05-06 18:15:03 +0900

    okok

--- WhatsUpJava/src/org/sepia/ch01/Out.java
+++ WhatsUpJava/src/org/sepia/ch01/Out.java
@@ -5,5 +5,6 @@ public class Out {
 		System.out.println("Hello World");
 		System.out.println("2019-05-06 18:05");
 		System.out.println("2019-05-06 18:11 git 충돌시에는 어떻게 표시를 해야 하죠?");
+		System.out.println("다시 추가합니다.");
 	}
 }
