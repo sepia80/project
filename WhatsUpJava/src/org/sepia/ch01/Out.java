d05d41322c8de7c7e2e5002a08f640908a6a3b6c
sepia80 <sepia80@naver.com>
2019-05-06 18:22:26 +0900

    dd

--- WhatsUpJava/src/org/sepia/ch01/Out.java
+++ WhatsUpJava/src/org/sepia/ch01/Out.java
@@ -2,7 +2,7 @@ package org.sepia.ch01;
 
 public class Out {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World 2");
 		System.out.println("2019-05-06 18:05");
 		System.out.println("2019-05-06 18:11 git 충돌시에는 어떻게 표시를 해야 하죠?");
 		System.out.println("다시 추가합니다.");
